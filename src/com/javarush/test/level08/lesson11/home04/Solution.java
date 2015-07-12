55199a9c7101971a7037ef591f48c2fa0104cf66
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-07-12 10:04:18 +0300

    Solved task (8/11/04
    level08.lesson11.home04

---
+++ src/com/javarush/test/level08/lesson11/home04/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.test.level08.lesson11.home04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/* Минимальное из N чисел
+1. Ввести с клавиатуры число N.
+2. Считать N целых чисел и заполнить ими список - метод getIntegerList.
+3. Найти минимальное число среди элементов списка - метод getMinimum.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception {
+        List<Integer> integerList = getIntegerList();
+        System.out.println(getMinimum(integerList));
+    }
+
+    public static int getMinimum(List<Integer> array) {
+        return Collections.min(array);
+    }
+
+    public static List<Integer> getIntegerList() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        for (int i = 0; i < number; i++)
+        {
+            arrayList.add(scanner.nextInt());
+        }
+        return arrayList;
+    }
+}
