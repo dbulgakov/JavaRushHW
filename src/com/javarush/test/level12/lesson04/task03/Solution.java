24634129d0caf743279abb05344e3ba134f0b765
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-07-14 15:11:45 +0300

    Solved task (12/4/3)
    level12.lesson04.task03

---
+++ src/com/javarush/test/level12/lesson04/task03/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static void print (int i)
+    {
+
+    }
+
+    public static void print (String s)
+    {
+
+    }
+
+    public static void print (float f)
+    {
+
+    }
+
+    public static void print (double b)
+    {
+
+    }
+
+    public static void print (byte b)
+    {
+
+    }
+}
