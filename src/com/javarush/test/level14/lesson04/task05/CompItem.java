050e6dc9151bd5af04dcf73405cd490e31098339
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-07-18 22:12:43 +0300

    Solved task (14/8/5)
    level14.lesson08.home05

---
+++ src/com/javarush/test/level14/lesson04/task05/CompItem.java
@@ -0,0 +1,6 @@
+package com.javarush.test.level14.lesson04.task05;
+
+public interface CompItem
+{
+    String getName();
+}
---
+++ src/com/javarush/test/level14/lesson08/home05/Mouse.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level14.lesson08.home05;
+
+/**
+ * Created by Дмитрий on 7/18/2015.
+ */
+public class Mouse implements CompItem
+{
+    @Override
+    public String getName()
+    {
+        return "Mouse";
+    }
+}
